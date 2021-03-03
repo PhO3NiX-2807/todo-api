bc5baa8ced674485fa052372fe26d5ad11b1630a
Kshitij Prabhu <kshitij.prabhu@somaiya.edu>
2021-03-03 13:05:12 +0530

    Add PUT todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+const { response } = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = []
@@ -56,6 +57,37 @@ app.delete('/todos/:id',function(req,res){
     }
 });
 
+app.put('/todos/:id',function(req,res){
+    var body = _.pick(req.body , 'description' , 'completed');
+    var validAttributes = {};
+
+    var todoId = parseInt(req.params.id,10);
+    var matchedTodo = _.findWhere(todos,{id:todoId});
+
+    if(!matchedTodo){
+        return res.send(404).send();
+    }
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+
+    }else if (body.hasOwnProperty('completed')){
+        return response.status(400).send();
+    }
+
+    if(body.hasOwnProperty('description')  && _.isString(body.description)  && body.description.trim().length > 0){
+        validAttributes.description = body.description;
+    }else if (body.hasOwnProperty('description')){
+        return response.status(400).send();
+    }
+
+    
+    _.extend(matchedTodo,validAttributes);
+
+    res.json(matchedTodo);
+
+
+
+})
 
 app.listen(PORT,function(){
     console.log('Express Port listening on' + PORT + '!');
